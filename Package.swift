61786317b8cfb6a79160ccd50c84fd48c75e7c44
Chris Hatton <christopherhattonuk@gmail.com>
2017-03-15 21:26:00 +1100

    Sync

---
+++ Package.swift
@@ -0,0 +1,20 @@
+//
+//  Package.swift
+//  SwiftImageDemo-iOS
+//
+//  Created by Christopher Hatton on 09/10/2016.
+//  Copyright © 2016 Chris Hatton. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftImageDemo-iOS”
+    targets: [],
+    dependencies: [
+	.Package(url: “https://github.com/chris-hatton/SwiftImage.git“,      majorVersion: 1),
+	.Package(url: “https://github.com/chris-hatton/SwiftImage-iOS.git“,  majorVersion: 1),
+	.Package(url: “https://github.com/chris-hatton/SwiftVision.git“,     majorVersion: 1),
+	.Package(url: “https://github.com/chris-hatton/SwiftVision-iOS.git“, majorVersion: 1)
+    ]
+)
